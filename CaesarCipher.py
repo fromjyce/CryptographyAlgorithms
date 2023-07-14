7706c49a2647c5e00c20d14b0ca68a616cf13d0f
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-14 09:59:58 +0530

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,27 +1,30 @@
 import string
 
-def return_enc_key(i: str, lst : list[str],key: int) -> int:
-    return (lst.index(i)+key)%26
+
+def return_enc_key(i: str, lst: list[str], key: int) -> int:
+    return (lst.index(i) + key) % 26
+
 
 def main():
     lower_alphabets = list(string.ascii_lowercase)
     higher_alphabets = list(string.ascii_uppercase)
     newword = []
     key = int(input("Enter the Key: "))
-    if 1<=key<=13:
+    if 1 <= key <= 13:
         word = input("Enter the Word: ")
         if word.isalpha():
-            for i in list(word):
-                if i.islower() and i in lower_alphabets:
-                    enc_key = return_enc_key(i,lower_alphabets,key)
+            for i in word:
+                if i in lower_alphabets:
+                    enc_key = return_enc_key(i, lower_alphabets, key)
                     newword.append(lower_alphabets[enc_key])
-                elif i.isupper() and i in higher_alphabets:
-                    enc_key = return_enc_key(i,higher_alphabets,key)
+                elif i in higher_alphabets:
+                    enc_key = return_enc_key(i, higher_alphabets, key)
                     newword.append(higher_alphabets[enc_key])
-            print("".join(newword))
+            print("Encrypted Word: " + "".join(newword))
         else:
             print("Enter a valid word")
     else:
         print("Enter a valid key")
+
 
 main()
