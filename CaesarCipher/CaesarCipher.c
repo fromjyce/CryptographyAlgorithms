01cfe13491037219dea44b390c48d49e203d8e6d
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-25 14:06:08 +0530

    Update CaesarCipher.c

--- CaesarCipher/CaesarCipher.c
+++ CaesarCipher/CaesarCipher.c
@@ -5,6 +5,20 @@ int generate_enc_key(int num,int key)
     {
         return (num+key)%26;
     }
+void removeSpaces(char *str) {
+    char *dest = str;
+    char *src = str;
+    
+    while (*src != '\0') {
+        if (!isspace(*src)) {
+            *dest = *src;
+            dest++;
+        }
+        src++;
+    }
+    
+    *dest = '\0';
+}
 int main() {
     int key,j,num,enc_key;
     char word[100], new_word[100] = "";
@@ -19,6 +33,7 @@ int main() {
         {
             printf("Enter the word: ");
             scanf("%s", word);
+            removeSpaces(word);
             for (int i=0; i<strlen(word); i++)
                 {
                     for (j = 0; j < 26; j++) {
