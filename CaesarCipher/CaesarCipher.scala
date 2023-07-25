46dd3ffc29b53dd3385533137ddf0c049a931f68
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-25 09:02:18 +0530

    Update CaesarCipher.scala

--- CaesarCipher/CaesarCipher.scala
+++ CaesarCipher/CaesarCipher.scala
@@ -4,27 +4,15 @@ def ReturnEncKey(num: Int, key: Int) : Int = {
 
 def ReadPlainText(key : Int) : Unit = {
 	val stringBuilder = StringBuilder()
-	val lower_chars = ('a' to 'z').zipWithIndex
-	val upper_chars = ('A' to 'Z').zipWithIndex
+	val alphabets = ('A' to 'Z').zipWithIndex
 	print("Enter the plaintext: ");
 	var word = scala.io.StdIn.readLine();
-	for (char <- word.toList) {
-		if (char.isUpper) {
-			for ((alph, index) <- upper_chars) {
-				if (alph==char) {
-					var enc_key = ReturnEncKey(index, key)
-					var enc_char = upper_chars(enc_key)._1
-					stringBuilder.append(enc_char)
-				}
-			}
-		}
-		else {
-			for((alph, index) <- lower_chars) {
-				if (alph==char) {
-					var enc_key = ReturnEncKey(index, key)
-					var enc_char = lower_chars(enc_key)._1
-					stringBuilder.append(enc_char)
-				}
+	for (char <- word.toUpperCase.toList) {
+		for ((alph, index) <- alphabets) {
+			if (alph==char) {
+				var enc_key = ReturnEncKey(index, key)
+				var enc_char = alphabets(enc_key)._1
+				stringBuilder.append(enc_char)
 			}
 		}
 	}
