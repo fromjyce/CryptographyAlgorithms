05cfd292c5e100b87fc91f82ab0a945a59d0375a
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-25 14:18:50 +0530

    Update OneTimePadCipher.py

--- OneTimePadCipher/OneTimePadCipher.py
+++ OneTimePadCipher/OneTimePadCipher.py
@@ -6,8 +6,8 @@ random_key_index = []
 plain_text_index = []
 new_word_index = []
 new_word = []
-plain_text = input("Enter the plaintext: ")
-random_key = input("Enter a random key: ")
+plain_text = input("Enter the plaintext: ").lower()
+random_key = input("Enter a random key: ").lower()
 if len(plain_text) == len(random_key):
     for j in random_key:
         if j in alphabets:
@@ -16,7 +16,7 @@ if len(plain_text) == len(random_key):
         if j in alphabets:
             plain_text_index.append(alphabets.index(j))
     for i, j in zip(random_key_index, plain_text_index):
-        new_word_index.append(i + j if i + j <= 25 else (i + j) - 26)
+        new_word_index.append((i+j)%26)
     for j in new_word_index:
         new_word.append(alphabets[j])
-    print("Encrypted Word: " + "".join(new_word))
+    print("Encrypted Word: " + "".join(new_word).upper())
