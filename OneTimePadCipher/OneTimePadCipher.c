9fa9d6f58f85787fe1723b81ab0426589028bca8
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-07-25 14:17:46 +0530

    Update OneTimePadCipher.c

--- OneTimePadCipher/OneTimePadCipher.c
+++ OneTimePadCipher/OneTimePadCipher.c
@@ -1,48 +1,57 @@
-// Improvedment of Vernam Cipher
+// Improvement of Vernam Cipher
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+void removeSpaces(char *str) {
+    char *dest = str;
+    char *src = str;
+    
+    while (*src != '\0') {
+        if (!isspace(*src)) {
+            *dest = *src;
+            dest++;
+        }
+        src++;
+    }
+    
+    *dest = '\0';
+}
 
 int main() {
     int num;
     char alphabets[26], plain_text[100], random_key[100];
     printf("Enter a plain text: ");
-    scanf("%s", plain_text);
+    scanf(" %[^\n]", plain_text);
     printf("Enter a random key: ");
-    scanf("%s", random_key);
+    scanf(" %[^\n]", random_key);
+    removeSpaces(plain_text);
+    removeSpaces(random_key);
     int length = strlen(plain_text);
     int plain_text_arr[length];
     int random_key_arr[length];
     int new_word_arr[length];
     char new_word[100] = "";
     for (int j = 0; j < 26; j++) {
-        alphabets[j] = (char) tolower('A' + j);
+        alphabets[j] = (char)toupper('A' + j);
     }
-
     if (strlen(plain_text) == strlen(random_key)) {
         for (int i = 0; i < strlen(plain_text); i++) {
             for (int k = 0; k < 26; k++) {
-                if (plain_text[i] == alphabets[k]) {
+                if (toupper(plain_text[i]) == alphabets[k]) {
                     plain_text_arr[i] = k;
                 }
             }
         }
         for (int i = 0; i < strlen(random_key); i++) {
             for (int k = 0; k < 26; k++) {
-                if (random_key[i] == alphabets[k]) {
+                if (toupper(random_key[i]) == alphabets[k]) {
                     random_key_arr[i] = k;
                 }
             }
         }
         for (int i=0; i<length; i++) {
-            if (random_key_arr[i]+plain_text_arr[i]<26)
-                {
-                    new_word_arr[i] = random_key_arr[i]+plain_text_arr[i];
-                }
-            else
-                {
-                    new_word_arr[i] = (random_key_arr[i]+plain_text_arr[i])-26;
-                }
+            new_word_arr[i] = (random_key_arr[i]+plain_text_arr[i])%26;   
         }
         for (int i=0; i<length; i++)
             {
