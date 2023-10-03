fff3da1d79b3cacc2601ef1032cee34c7c409a8e
jaya2 <jaya2004kra@gmail.com>
2023-10-03 14:09:27 +0530

    addec c file

---
+++ OneTimePadCipher/Decryption/OneTimePadCipher.c
@@ -0,0 +1,67 @@
+// Improvement of Vernam Cipher
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void removeNonAlphabetic(char *str) {
+    char *dest = str;
+    char *src = str;
+
+    while (*src != '\0') {
+        if (isalpha(*src)) {
+            *dest = toupper(*src); // Convert to uppercase
+            dest++;
+        }
+        src++;
+    }
+
+    *dest = '\0';
+}
+
+int main() {
+    int num;
+    char alphabets[26], cipher_text[100], random_key[100];
+    printf("Enter a cipher text: ");
+    scanf(" %[^\n]", cipher_text);
+    printf("Enter a random key: ");
+    scanf(" %[^\n]", random_key);
+    removeNonAlphabetic(cipher_text);
+    removeNonAlphabetic(random_key);
+    int length = strlen(cipher_text);
+    int cipher_text_arr[length];
+    int random_key_arr[length];
+    int new_word_arr[length];
+    char new_word[100] = "";
+    for (int j = 0; j < 26; j++) {
+        alphabets[j] = (char)toupper('A' + j);
+    }
+    if (strlen(cipher_text) == strlen(random_key)) {
+        for (int i = 0; i < strlen(cipher_text); i++) {
+            for (int k = 0; k < 26; k++) {
+                if (toupper(cipher_text[i]) == alphabets[k]) {
+                    cipher_text_arr[i] = k;
+                }
+            }
+        }
+        for (int i = 0; i < strlen(random_key); i++) {
+            for (int k = 0; k < 26; k++) {
+                if (toupper(random_key[i]) == alphabets[k]) {
+                    random_key_arr[i] = k;
+                }
+            }
+        }
+        for (int i=0; i<length; i++) {
+            new_word_arr[i] = (cipher_text_arr[i] - random_key_arr[i])%26;   
+        }
+        for (int i = 0; i < length; i++) {
+            new_word[i] = alphabets[new_word_arr[i]];
+            }
+        new_word[length] = '\0';
+        printf("Plain Text: %s", new_word);
+    }
+    else {
+        printf("Different Lengths");
+    }
+    
+    return 0;
+}
