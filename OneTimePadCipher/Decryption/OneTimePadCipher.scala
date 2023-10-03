f216389af31919ffc19a1ff63a37bdad1c6757fd
jaya2 <jaya2004kra@gmail.com>
2023-10-03 14:24:48 +0530

    added scala file

---
+++ OneTimePadCipher/Decryption/OneTimePadCipher.scala
@@ -0,0 +1,35 @@
+def index_giving(text: String, array: IndexedSeq[(Char, Int)]): List[Int] = {
+  text.toUpperCase.toList.flatMap { char =>
+    array.find(_._1 == char).map(_._2)
+  }
+}
+
+def enc_key_giving(cipher_text: List[Int], random_text: List[Int], length: Int): List[Int] = {
+  (cipher_text zip random_text).map { case (cipherIndex, randomIndex) =>
+    (cipherIndex - randomIndex + 26) % 26
+  }
+}
+
+def OneTimePadCipher(cipher_text: String, random_text: String): String = {
+  if (random_text.length() >= cipher_text.length()) {
+    val stringBuilder = new StringBuilder()
+    val alphabets = ('A' to 'Z').zipWithIndex
+    val cipher_text_index = index_giving(cipher_text, alphabets)
+    val random_text_index = index_giving(random_text, alphabets)
+    val enc_word_index = enc_key_giving(cipher_text_index, random_text_index, cipher_text.length())
+    for (i <- enc_word_index) {
+      stringBuilder.append(alphabets(i)._1)
+    }
+    stringBuilder.toString
+  } else {
+    "Invalid Key"
+  }
+}
+
+@main def main(): Unit = {
+  print("Enter Cipher text: ")
+  val cipher_text = scala.io.StdIn.readLine()
+  print("Enter Randomtext: ")
+  val random_text = scala.io.StdIn.readLine()
+  println(OneTimePadCipher(cipher_text, random_text))
+}
