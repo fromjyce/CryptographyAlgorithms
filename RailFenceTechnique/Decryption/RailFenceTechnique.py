9e978c076e0e8ef59ebc283577ff84af278002ef
jaya2 <jaya2004kra@gmail.com>
2023-12-08 20:45:47 +0530

    added railfence decrypt in python

--- RailFenceTechnique/Decryption/RailFenceTechnique.py
+++ RailFenceTechnique/Decryption/RailFenceTechnique.py
@@ -1,19 +1,19 @@
-def separation_odd_even(cipher_text):
-    new_word_even = []
-    new_word_odd = []
-    for i in range(0, len(cipher_text)):
-        if i % 2 == 0:
-            new_word_even.append(cipher_text[i])
-        else:
-            new_word_odd.append(cipher_text[i])
-    new_word_even.extend(new_word_odd)
-    return new_word_even
+def railFenceDecryption(cipherText):
+    n = len(cipherText)
+    half_length = n // 2
+    even_chars = cipherText[:half_length]
+    odd_chars = cipherText[half_length:]
+
+    decrypted_text = []
+    for i in range(half_length):
+        decrypted_text.append(even_chars[i])
+        decrypted_text.append(odd_chars[i])
+    if n % 2 == 1:
+        decrypted_text.append(even_chars[-1])
+
+    return "".join(decrypted_text)
 
 
-cipher_text = input("Enter the cipher text: ").replace(" ", "").lower()
-new_word_list = []
-new_word_list = separation_odd_even(cipher_text)
-new_word_list = separation_odd_even(''.join(new_word_list))
-
-
-print("Decrypted Word: " + "".join(new_word_list).upper())
+cipher_text = input("Enter the encrypted text: ")
+decrypted_text = railFenceDecryption(cipher_text)
+print("Decrypted Word:", decrypted_text.upper())
